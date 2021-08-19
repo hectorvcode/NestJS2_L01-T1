5a403f0cac67064a2fe6fd007a1c8108190706c2
Hector Vasquez <hectorfabiovasquez@gmail.com>
2021-08-19 17:22:43 -0500

    Modify AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,16 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [],
+  imports: [NestjsKnexModule.register({
+      client: ‘mysql’,
+      connection: {
+          host: 'remotemysql.com',
+          user: 'PrVenWSklm',
+          password: 'H9p5XAvf8U',
+          database: 'PrVenWSklm',
+          port: 3306,
+      },
+  })],
   controllers: [AppController, UserController],
   providers: [AppService],
 })

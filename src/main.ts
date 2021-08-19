df733dd0ec90d390dc3dc7a7df353907f4f1551c
Hector Vasquez <hectorfabiovasquez@gmail.com>
2021-08-19 15:52:15 -0500

    Configured to heroku

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+
+  app.enableCors(options);
+  const PORT = parseInt(process.env.PORT, 10) || 3000;
+  Logger.log(`App running on: ${PORT}`);
+  Logger.debug(`App running on: ${PORT}`);
+  Logger.error(`App running on: ${PORT}`);
+  Logger.warn(`App running on: ${PORT}`);
+  console.log(`App running on: ${PORT}`);
+  await app.listen(PORT);
 }
 bootstrap();

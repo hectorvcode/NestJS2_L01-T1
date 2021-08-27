445cec53c840e07c986f7e3950ea59f75a8e25b9
hectorvcode <hectorfabiovasquez@gmail.com>
2021-08-27 17:26:09 -0500

    Delete old documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthGuard } from './auth.guard';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
+      rootPath: join(__dirname, '..', 'docs'),
     }),
     NestjsKnexModule.register({
       client: 'mysql',
